fb4b3feaa6e18573248a0163cc3dfc94718f8500
LeoTP <pupdoodle@gmail.com>
2020-11-20 12:33:31 +1300

    Scenes added
    Main menu, chapters, Levels

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void btn_scene_change(string scene_name)
+    {
+        SceneManager.LoadScene(scene_name);
+    }
+}
