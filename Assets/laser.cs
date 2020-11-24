6d8661446c75f618d4b19f65234b0faed92ce7e0
LeoTP <pupdoodle@gmail.com>
2020-11-24 14:41:05 +1300

    pushin

---
+++ Assets/laser.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laser : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    public Transform laserHit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.enabled = false;
+        lineRenderer.useWorldSpace = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up);
+        Debug.DrawLine(transform.position, hit.point);   
+    }
+}
