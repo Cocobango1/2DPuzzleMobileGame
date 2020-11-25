67a022be5378e98bfb18c4547e6160401db29afa
L <lawnhawke@gmail.com>
2020-11-25 21:36:14 +1300

    Quite a bit

--- Assets/laser.cs
+++ Assets/laser.cs
@@ -1,12 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class laser : MonoBehaviour
 {
     private LineRenderer lineRenderer;
     public Transform laserHit;
-    // Start is called before the first frame update
+
     void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
@@ -14,7 +12,6 @@ public class laser : MonoBehaviour
         lineRenderer.useWorldSpace = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up);
