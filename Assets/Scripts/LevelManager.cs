69f93a2de308b0add7e7688e094942abe675d73e
LeoTP1226 <pupdoodle@gmail.com>
2020-11-27 15:00:53 +1300

    Level management

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager instance;
+    public int[] level;
+    public GameObject[] levels;
+    // Start is called before the first frame update
+    void Start()
+    {
+       // level[0] = 1;
+    }
+    private void Awake()
+    {
+        instance = this;
+
+        if (LevelManager.instance != this)
+            Destroy(LevelManager.instance);
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < level.Length; i++)
+        {
+            if (level[i] == 1 && levels[i] != null)
+            {
+                levels[i].SetActive(true);
+            }
+            else if (level[i] != 1 && levels[i] != null)
+            {
+                levels[i].SetActive(false);
+            }
+        }
+    }
+}
--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -6,7 +6,27 @@ using UnityEngine.UI;
 
 public class SceneChange : MonoBehaviour
 {
+<<<<<<< Updated upstream
     public void btn_scene_change()
+=======
+    public LevelManager GM;
+    public string scene_name;
+    public int num;
+    private void Start()
+    {
+        //GM= 
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            LevelManager.instance.level[num] = 1;
+            SceneManager.LoadScene(scene_name);
+        }
+    }
+    // Start is called before the first frame update
+    public void btn_scene_change(string scene_name)
+>>>>>>> Stashed changes
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
